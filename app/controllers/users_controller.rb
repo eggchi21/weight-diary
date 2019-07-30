4bfbb593946739b55b00710d6711451fb80c65d8
eggchi21 <eggchi21@yahoo.co.jp>
2019-07-30 20:01:44 +0900

    mod routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @user=User.find(current_user.id)
+    @diaries=Diary.where(user_id:current_user.id).order('created_at ASC')
+    @ids=@diaries.map(&:id)
+    @weights=@diaries.map(&:weight)
+    @dates=@diaries.map{|diary| diary.created_at.strftime('%Y/%m/%d') }
+  end
+
   def show
     @user=User.find(params[:id])
     @diaries=Diary.where(user_id:params[:id]).order('created_at ASC')
@@ -7,5 +16,4 @@ class UsersController < ApplicationController
     @weights=@diaries.map(&:weight)
     @dates=@diaries.map{|diary| diary.created_at.strftime('%Y/%m/%d') }
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'diaries#index'
-  resources :diaries
-  resources :users, only: [:show,:edit,:update]
+  root 'users#index'
+  # resources :diaries
+  resources :users, only: [:index,:show,:edit,:update] do
+    resources :diaries
+  end
 end
